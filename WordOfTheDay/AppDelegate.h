538528430cbbac1aaff76b6dfee13504f10d48ee
LittleFin <littlefin@LittleFin-Pro.local>
2014-06-23 13:19:34 -0500

    Initial Commit

---
+++ WordOfTheDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordOfTheDay
+//
+//  Created by LittleFin on 6/23/14.
+//  Copyright (c) 2014 Michael Dattolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordOfTheDay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WordOfTheDay
+//
+//  Created by LittleFin on 6/23/14.
+//  Copyright (c) 2014 Michael Dattolo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
